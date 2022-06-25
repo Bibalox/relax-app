46b8bddddd3e929054d725dca3e40858c2eeba1a
JB Bal <bal@abvent.fr>
2022-06-25 18:25:31 +0200

    🚧 Try the audio features and fix the fullscreen

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <audio
+    controls
+    src="./assets/goutte.mp3"
+    type="audio/mpeg"
+  />
+  <button @click="playAudio">Test</button>
 </template>
 
 <script>
@@ -10,22 +16,30 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  methods: {
+    playAudio () {
+      const audio = new Audio('./assets/goutte.mp3')
+      audio.play()
+      console.log('Audio played')
+    }
   }
 }
 </script>
 
 <style lang="scss">
   body {
+    background: linear-gradient(180deg, #80C3F4 0%, #70B9EE 100%);
     margin: 0;
     padding: 0;
   }
 
   #app {
-    background: linear-gradient(180deg, #80C3F4 0%, #70B9EE 100%);
     font-family: Avenir, Helvetica, Arial, sans-serif;
     text-align: center;
     color: white;
     height: 100vh;
     overflow: hidden;
+    position: fixed;
   }
 </style>
