cb6682fd64efb5459a3ba5253bb681a9dd80ad16
JB Bal <bal@abvent.fr>
2022-06-25 17:51:15 +0200

    💄 🚧 UI tests

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,17 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+    background: linear-gradient(180deg, #80C3F4 0%, #70B9EE 100%);
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+    color: white;
+    height: 100vh;
+    overflow: hidden;
+  }
 </style>
