2136b5c69e65a67d9d081b4a1642105fa9222ade
JB Bal <bal@abvent.fr>
2022-06-29 16:18:09 +0200

    🍱 🐛 Minor fixes

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,13 @@
 module.exports = {
   pwa: {
     manifestOptions: {
-      name: "Relax",
+      name: "Time to relax",
+      short_name: "Relax",
       display: "fullscreen",
       start_url: "/?fullscreen=true",
       theme_color: "#5DADE8",
     },
+    name: "Relax",
     appleMobileWebAppStatusBarStyle: "black-translucent",
   }
 }
