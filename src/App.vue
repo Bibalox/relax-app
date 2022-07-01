20ca6330630967ad1d05b7f70cdb910417398fec
JB Bal <bal@abvent.fr>
2022-07-01 12:22:26 +0200

    ⚰️ Remove unused code

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,6 @@
   </router-view>
 </template>
 
-<script>
-export default {
-  mounted () {
-    window.addEventListener("scroll", (e) => {
-      e.preventDefault()
-      window.scrollTo(0, 0)
-    })
-  }
-}
-</script>
-
 <style lang="scss">
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap');
   
