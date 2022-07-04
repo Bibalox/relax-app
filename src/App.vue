330ba610b108bca60ec1bdfaa1bd55fb354c8d64
JB Bal <bal@abvent.fr>
2022-07-04 15:34:45 +0200

    ⚰️ Cleanup some unused code

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,6 @@
 <template>
-  <!-- <router-view v-slot="{ Component }" @touchstart="preventSwipeNavigation">
-    <transition
-      name="route-transition"
-      mode="out-in"
-    >
-      <component :is="Component"/>
-    </transition>
-  </router-view> -->
   <router-view />
 </template>
-
-<script>
-export default {
-  methods: {
-    preventSwipeNavigation (e) {
-      if (e.pageX > 32 && e.pageX < window.innerWidth - 32) return
-      e.preventDefault()
-    }
-  }
-}
-</script>
 
 <style lang="scss">
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap');
@@ -66,32 +47,4 @@ export default {
     height: 100%;
     justify-content: center;
   }
-
-  // .route-transition {
-  //   &-enter-active {
-  //     animation: fade-in .5s;
-  //   }
-
-  //   &-leave-active {
-  //     animation: fade-out .5s;
-  //   }
-
-  //   @keyframes fade-in {
-  //     0% {
-  //       opacity: 0;
-  //     }
-  //     100% {
-  //       opacity: 1;
-  //     }
-  //   }
-
-  //   @keyframes fade-out {
-  //     0% {
-  //       opacity: 1;
-  //     }
-  //     100% {
-  //       opacity: 0;
-  //     }
-  //   }
-  // }
 </style>
