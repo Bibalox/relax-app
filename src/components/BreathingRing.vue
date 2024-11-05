1838f37fd1151733df1f76113ee87bf9e0a1ccbf
JB Bal <bal@abvent.fr>
2024-11-05 09:27:45 +0100

    ✨ Add the breathing ring component

--- src/components/BreathingRing.vue
+++ src/components/BreathingRing.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+const props = defineProps<{
+  breathing: boolean
+}>()
 
+defineEmits(['click'])
 </script>
 
 <template>
@@ -8,51 +12,69 @@
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     class="breathing-ring"
-    :class="{ 'breathing-ring--breathing' : breathing }"
-    @click="click"
+    :class="{ 'breathing-ring--breathing' : props.breathing }"
+    @click="$emit('click')"
   >
     <circle
       class="breathing-ring__outer-ring"
-      cx="160" cy="160" r="160"
+      cx="160" cy="160"
+      r="160"
       shape-rendering="geometricPrecision"
       fill="url(#outer-ring-gradient)"
     />
     <circle
       class="breathing-ring__center-ring"
-      cx="160" cy="160" r="107"
+      cx="160" cy="160"
+      r="107"
       shape-rendering="geometricPrecision"
       fill="url(#center-ring-gradient)"
     />
     <circle
       class="breathing-ring__inner-ring"
-      cx="160" cy="160" r="50"
+      cx="160" cy="160"
+      r="50"
       shape-rendering="geometricPrecision"
       fill="url(#inner-ring-gradient)"
     />
     <defs>
       <radialGradient
         id="outer-ring-gradient"
-        cx="0" cy="0" r="1"
+        cx="0" cy="0"
+        r="1"
         gradientUnits="userSpaceOnUse" gradientTransform="translate(160 160) scale(160)"
       >
-        <stop offset="0.7" stop-color="white" stop-opacity="0"/>
-        <stop offset="1" stop-color="white" stop-opacity="0.19"/>
+        <stop
+          offset="0.7" stop-color="white"
+          stop-opacity="0"
+        />
+        <stop
+          offset="1" stop-color="white"
+          stop-opacity="0.19"
+        />
       </radialGradient>
       <radialGradient
         id="center-ring-gradient"
-        cx="0" cy="0" r="1"
+        cx="0" cy="0"
+        r="1"
         gradientUnits="userSpaceOnUse" gradientTransform="translate(160 160) scale(107)"
       >
-        <stop offset="0.6" stop-color="white" stop-opacity="0" />
-        <stop offset="1" stop-color="white" stop-opacity="0.19"/>
+        <stop
+          offset="0.6" stop-color="white"
+          stop-opacity="0"
+        />
+        <stop
+          offset="1" stop-color="white"
+          stop-opacity="0.19"
+        />
       </radialGradient>
       <radialGradient
         id="inner-ring-gradient"
-        cx="0" cy="0" r="1"
+        cx="0" cy="0"
+        r="1"
         gradientUnits="userSpaceOnUse" gradientTransform="translate(160 160) rotate(90) scale(50 50)"
       >
-        <stop offset="0.5" stop-color="white"/>
-        <stop offset="1" stop-color="#F9F9F9"/>
+        <stop offset="0.5" stop-color="white" />
+        <stop offset="1" stop-color="#F9F9F9" />
       </radialGradient>
     </defs>
   </svg>
@@ -63,6 +85,10 @@
   cursor: pointer;
   height: 320px;
   width: 320px;
+
+  &__inner-ring {
+    cursor: pointer;
+  }
 
   &--breathing &__inner-ring {
     animation: innerRingBreathing 5s .2s alternate infinite ease-in-out;
--- src/views/BreatheView.vue
+++ src/views/BreatheView.vue
@@ -1,17 +1,27 @@
 <script setup lang="ts">
+import { reactive } from 'vue'
 
+import BreathingRing from '@components/BreathingRing.vue'
+import PrimaryButton from '@components/PrimaryButton.vue'
+
+const state = reactive ({
+  breathing: false
+})
+
+const startBreathing = () => {
+  state.breathing = !state.breathing
+}
 </script>
 
 <template>
   <div class="breathe-view">
     <main class="breathe-view__main">
-      <!-- <BreathingRing :breathing="breathing" @click="startBreathing" /> -->
-      <BreathingRing />
+      <breathing-ring :breathing="state.breathing" @click="startBreathing" />
 
       <footer class="breathe-view__footer">
         <transition name="fade" mode="out-in">
           <div
-            v-if="breathing"
+            v-if="state.breathing"
             class="breathe-view__instructions"
           >
             <span class="breathe-view__label">
@@ -21,10 +31,10 @@
               Breathe out
             </span>
           </div>
-          <PrimaryButton
+          <primary-button
             v-else
             label="Start"
-            @clickButton="startBreathing"
+            @click="startBreathing"
           />
         </transition>
       </footer>
@@ -34,7 +44,11 @@
       class="breathe-view__back-button"
       to="/"
     >
-      <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg
+        width="16" height="16"
+        viewBox="0 0 16 16" fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
         <path
           d="M10.7071 2.70711C11.0976 2.31658 11.0976 1.68342 10.7071 1.29289C10.3166 0.902369 9.68342 0.902369 9.29289 1.29289L10.7071 2.70711ZM4 8L3.29289 7.29289C2.90237 7.68342 2.90237 8.31658 3.29289 8.70711L4 8ZM9.29289 14.7071C9.68342 15.0976 10.3166 15.0976 10.7071 14.7071C11.0976 14.3166 11.0976 13.6834 10.7071 13.2929L9.29289 14.7071ZM9.29289 1.29289L3.29289 7.29289L4.70711 8.70711L10.7071 2.70711L9.29289 1.29289ZM3.29289 8.70711L9.29289 14.7071L10.7071 13.2929L4.70711 7.29289L3.29289 8.70711Z"
           fill="var(--secondary--default)"
@@ -72,6 +86,7 @@
     top: 24px;
     transition: background-color .4s ease-in-out;
     left: 32px;
+    -webkit-user-select: none;
     user-select: none;
 
     &:hover {
@@ -107,11 +122,13 @@
   &__label {
     animation: fade-in-out 10s ease-in-out infinite both;
     color: var(--secondary--default);
+    cursor: default;
     font-family: 'Poppins', sans-serif;
     font-size: 1.6rem;
     font-weight: 500;
     opacity: 0;
     position: absolute;
+    -webkit-user-select: none;
     user-select: none;
 
     &--animation-delayed {
